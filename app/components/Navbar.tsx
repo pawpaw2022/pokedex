b408d4abe963a0bc5c1153e8b698e6b600d8ef60
Paul Lu <paul.l.sining@gmail.com>
2023-04-07 12:46:45 -0700

    recreate with next 13

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from "react";
+import styles from "./Navbar.module.scss";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className={styles.navbar}>hello world</div>
+    </div>
+  );
+}
