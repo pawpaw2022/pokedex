b354c9f41ae08e48f4d390a7984ba52817316285
Paul Lu <sininglu@sandiego.edu>
2023-04-09 15:29:06 -0700

    working on

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,28 +7,28 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className={styles.container}>
-      <Image src="/pokelogo.svg" alt="Pokemon Logo" height={100} width={500} />
-      <div className={styles.navbar}>
-        <div className={styles.navbar_opt}>
+    <div className="flex flex-col justify-center items-center mt-6">
+      <Image src="/pokelogo.svg" alt="Pokemon Logo" height={100} width={300} />
+      <div className="flex bg-white mx-4 rounded-md">
+        <div className="bold uppercase mx-2 my-1 hover:scale-105 ease-in-out">
             <Link href="#">Pokemon</Link>
         </div>
-        <div className={styles.navbar_opt}>
+        <div className="">
             <Link href="#">Type Chart</Link>
         </div>
-        <div className={styles.navbar_opt}>
+        <div className="">
             <Link href="#">Moves</Link>
         </div>
-        <div className={styles.navbar_opt}>
+        <div className="">
             <Link href="#">TM</Link>
         </div>
-        <div className={styles.navbar_opt}>
+        <div className="">
             <Link href="#">Ability</Link>
         </div>
-        <div className={styles.navbar_opt}>
+        <div className="">
             <Link href="#">Nature</Link>
         </div>
-        <div className={styles.navbar_opt}>
+        <div className="">
             <Link href="#">Berry</Link>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 /** @format */
 
-import { Inter } from "next/font/google";
-import styles from "./page.module.scss";
 import PokeCard from "./components/PokeCard";
-
-const inter = Inter({ subsets: ["latin"] });
 
 const gens = {
   1: [1, 151],
@@ -16,10 +12,9 @@ const gens = {
   7: [722, 809],
   8: [810, 898],
 };
-  
 
 async function getData(gen: number): Promise<Pokemon[]> {
-  let allPokemon: Pokemon[] = [];  
+  let allPokemon: Pokemon[] = [];
 
   for (let i = gens[gen][0]; i <= gens[gen][1]; i++) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
@@ -38,13 +33,12 @@ export default async function Home() {
   const allPokemon = await getData(1);
 
   return (
-    <main className={styles.main}>
-      <p className="bold text-xl bg-red-700" >Hello tailwind</p>
-      <div className={styles.grid}>
+    <div className="w-full">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
         {allPokemon.map((pokemon) => {
           return <PokeCard key={pokemon.id} pokemon={pokemon} />;
         })}
       </div>
-    </main>
+    </div>
   );
 }
