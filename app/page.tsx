0574ff61cbd32e2e9201906c190b1629d1830f2e
Paul Lu <sininglu@sandiego.edu>
2023-04-13 14:48:30 -0700

    include type icon svg files

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 /** @format */
 // import redis from "redis";
 
+import Image from "next/image";
+
 
 
 export default function Home() {
@@ -8,6 +10,7 @@ export default function Home() {
   return (
     <>
       <h1>Pokemon Home</h1>
+      <img className="w-full" src="/type-icons/normal.svg" />
     </>
   );
 }
