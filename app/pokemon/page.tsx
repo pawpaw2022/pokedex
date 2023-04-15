c8f4c0f4c8f09a94c3fdf7698113e7d55f91ab75
Paul Lu <paul.l.sining@gmail.com>
2023-04-15 14:21:04 -0700

    tried intersection oberserver

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -1,21 +1,12 @@
 /** @format */
 import React from "react";
 import { getPokemonData } from "../utils/datafetch";
-import PokeCard from "./components/PokeCard";
-import PokeCardSkeleton from "./components/PokeCardSkeleton";
+import Pokepage from "./components/Pokepage";
 
 export default async function Pokemon() {
-  const gen = 1;
-  const pokemons = await getPokemonData(gen);
-
   return (
     <>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-        {pokemons.map((pokemon) => {
-          return <PokeCard key={pokemon.id} pokemon={pokemon} gen={gen} />;
-          // return <PokeCardSkeleton />;
-        })}
-      </div>
+      <Pokepage />
     </>
   );
 }
