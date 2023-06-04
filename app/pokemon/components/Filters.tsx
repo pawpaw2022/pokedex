1c34ca1f3198f6ec8d6f4563caf5fbbfba1e1be6
Paul Lu <paul.l.sining@gmail.com>
2023-06-04 15:48:49 -0700

    added filters back

--- app/pokemon/components/Filters.tsx
+++ app/pokemon/components/Filters.tsx
@@ -1,10 +1,11 @@
 /** @format */
 'use client'
 import React from "react";
-import GenFilter from "./filters/GenFilter";
-import TypeFilter from "./filters/TypeFilter";
+
 import DarkModeToggle from "./filters/DarkModeToggle";
 import SearchBar from "./filters/SearchBar";
+import GenFilter from './filters/GenFilter';
+import TypeFilter from "./filters/TypeFilter";
 
 type Props = {
   handleGenFilter: (gen: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8) => void;
@@ -18,8 +19,8 @@ export default function Filters({ handleGenFilter, handleTypeFilter, handleSearc
   return (
     <>
       <div className="flex flex-row justify-center items-center mb-4">
-        {/* <GenFilter handleGenFilter={handleGenFilter} />
-        <TypeFilter handleTypeFilter={handleTypeFilter} /> */}
+        <GenFilter handleGenFilter={handleGenFilter} />
+        <TypeFilter handleTypeFilter={handleTypeFilter} />
         <SearchBar handleSearch={handleSearch} searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         <DarkModeToggle />
       </div>
---
+++ app/pokemon/components/filters/TypeFilter.tsx
@@ -0,0 +1,72 @@
+/** @format */
+
+import { typeCode } from "@/app/utils/config";
+import React from "react";
+
+type Props = {
+  handleTypeFilter: (type: string) => void;
+};
+
+export default function TypeFilter({ handleTypeFilter }: Props) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const [currentType, setCurrentType] = React.useState("All");
+
+  const toggle = () => setIsOpen((prev) => !prev);
+
+  const handleClick = (id: number) => {
+    const type = typeCode.filter((t) => t.id === id)[0]["name"];    
+
+    setCurrentType(type);
+    handleTypeFilter(type);
+
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <div className="w-30 ml-2 md:hidden">
+        <p className="text-center">Type</p>
+        <div>
+          <button
+            type="button"
+            onClick={toggle}
+            className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          >
+            {currentType}
+            <svg
+              className="-mr-1 h-5 w-5 text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+        {isOpen && (
+          <div className="rounded-md bg-white shadow-lg ring-1 ring-black  ring-opacity-5 focus:outline-none ">
+            <div className="py-1">
+              {typeCode.map((type) => {
+                return (
+                  <div
+                    className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 cursor-pointer"
+                    onClick={() => handleClick(type.id)}
+                    key={type.id}
+                  >
+                    {type.name}
+                  </div>
+                );
+              })}
+
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
