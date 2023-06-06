c007a01f7f15ff2fd6169a87af1e93f804b04a64
Paul Lu <paul.l.sining@gmail.com>
2023-06-05 22:20:38 -0700

    change side type color

--- app/pokemon/components/filters/TypeSideBar.tsx
+++ app/pokemon/components/filters/TypeSideBar.tsx
@@ -3,52 +3,32 @@
 import { typeCode } from "@/app/utils/config";
 import Image from "next/image";
 import React from "react";
-
+import TypeSideBarTicket from "./TypeSideBarTicket";
 
 type Props = {
-    handleTypeFilter: (type: string) => void;
+  handleTypeFilter: (type: string) => void;
+};
+
+export default function TypeSideBar({ handleTypeFilter }: Props) {
+  const [currentType, setCurrentType] = React.useState<string>("all");
+
+  const handleClick = (type: string) => {
+    setCurrentType(type.toLowerCase());
+    handleTypeFilter(type);
   };
-
-
-export default function TypeSideBar({handleTypeFilter}: Props ) {
-
-    const [currentType, setCurrentType] = React.useState<string>("all");
-
-    const handleClick = (type: string) => {
-        setCurrentType(type.toLowerCase());
-        handleTypeFilter(type);
-    }
-
 
   return (
     <aside className="hidden md:block">
       <div className="flex list-none flex-col flex-wrap pl-0 mt-8">
         <div className="flex-grow text-center">
-          {typeCode.map((type) => {
+          {typeCode.map((t) => {
             return (
-              <div
-                className={`
-                  my-2 w-full flex border-x-0 border-b-2 border-t-0 border-transparent
-                  pl-2 pr-5 py-2 text-xs uppercase hover:bg-indigo-400 
-                  cursor-pointer rounded-r-lg shadow-lg`}
-                  style={{ backgroundColor: currentType === type.name.toLowerCase() ? type.color : type.color+60 }}
-
-                key={type.id}
-                onClick={() => handleClick(type.name)}
-              >
-                 {
-                    type.name.toLowerCase() !== 'all' && 
-                    <Image
-                    className="mr-2"
-                    src={`/type-icons/${type.name.toLowerCase()}.svg`}
-                    alt={"type icon"}
-                    width={18}
-                    height={18}
-                  />
-                 }
-                {type.name}
-
-              </div>
+              <TypeSideBarTicket
+                type={t}
+                handleClick={handleClick}
+                key={t.id}
+                currentType={currentType}
+              />
             );
           })}
         </div>
---
+++ app/pokemon/components/filters/TypeSideBarTicket.tsx
@@ -0,0 +1,57 @@
+/** @format */
+
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  type: {
+    name: string;
+    id: number;
+    color: string;
+    fid: number;
+  };
+  handleClick: (type: string) => void;
+  currentType: string;
+};
+
+export default function TypeSideBarTicket({
+  type,
+  handleClick,
+  currentType,
+}: Props) {
+  const [isHover, setIsHover] = React.useState<boolean>(false);
+
+  const toggleHover = () => {
+    setIsHover((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={`
+      my-2 w-full flex border-x-0 border-b-2 border-t-0 border-transparent
+      pl-2 pr-5 py-2 text-xs uppercase hover:bg-indigo-400 
+      cursor-pointer rounded-r-lg shadow-lg ease-linear transition-all duration-150`}
+      style={{
+        backgroundColor:
+          isHover || type.name.toLowerCase() === currentType
+            ? type.color
+            : "#B9D5FF80",
+      }}
+      onMouseEnter={() => toggleHover()}
+      onMouseLeave={() => toggleHover()}
+      key={type.id}
+      onClick={() => handleClick(type.name)}
+    >
+      {type.name.toLowerCase() !== "all" && (
+        <Image
+          className="mr-2"
+          src={`/type-icons/${type.name.toLowerCase()}.svg`}
+          alt={"type icon"}
+          width={18}
+          height={18}
+        />
+      )}
+      {type.name}
+    </div>
+  );
+}
